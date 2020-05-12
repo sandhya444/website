e03e4d0e8829d59c181cfdcbdf332e358b528c59
Srinivasa krishnan <SandhyaSrinivasa@srinivasas-mbp.attlocal.net>
2020-05-11 23:51:32 -0500

    add blog post

--- start.R
+++ start.R
@@ -1,5 +1,5 @@
 install.packages("blogdown")
-#run to install hugo; wait until finished![](/website/start_files/DNA1.jpg)
+#run to install hugo; wait until finished!
 blogdown::install_hugo()
 
 blogdown::new_site(theme="nurlansu/hugo-sustain")
